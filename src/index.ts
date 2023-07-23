e247c2f2ee787a19b30af8d7a5d0a1a458bf7f71
Mauricio <maumarcastil@gmail.com>
2023-07-23 00:46:31 -0500

    fix: add config typeorm,  initialize

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,28 @@ import express, { type Express, type Request, type Response } from 'express'
 
 import 'reflect-metadata'
 import { PORT } from './config/enviroments'
+import { PostgresDataSource } from './database/config'
+import rootRouter from './routes/index.routes'
 
 const app: Express = express()
-app.use(morgan('dev'))
 app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
+app.use('/api', rootRouter)
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT as string}`)
 })
+
+PostgresDataSource.initialize()
+  .then(() => {
+    console.log('⚡️[postgres]: Data Source has been initialized!')
+  })
+  .catch((err) => {
+    console.log('⚡️[postgres]: Error during Data Source initialization', err)
+  })
